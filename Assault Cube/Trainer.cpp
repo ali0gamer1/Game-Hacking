7fea482b63f52b3a9e902dd4bdaf562a499ea2a2
ali0gamer1 <69399548+ali0gamer1@users.noreply.github.com>
2021-07-17 20:46:43 +0430

    Update Trainer.cpp

--- Assault Cube/Trainer.cpp
+++ Assault Cube/Trainer.cpp
@@ -96,8 +96,6 @@ int main()
 
     if (modulebase == 0)
     {
-
-
         std::cerr<<"Error: game not running"<<'\n';
         return 1;
     }
@@ -116,25 +114,39 @@ int main()
     ammos.push_back(0x0);
 
 
+    std::vector<unsigned int> healthvec;
+    healthvec.push_back(0xf8);
+
+    uintptr_t healthaddr = findDMAaddr(hprocess, dynamicptr, healthvec);
+
+
     uintptr_t ammoaddr = findDMAaddr(hprocess, dynamicptr, ammos), tempammoaddr = 0;
     std::cout<<"Dynamic pointer base: "<< std::hex << dynamicptr<<'\n';
     std::cout<<"ammo addr:  " << ammoaddr<<'\n'<<std::dec;
-    int ammo = 0, temp = 0, newammo = 100;
-
+    int ammo = 0, tempammo = 0, newammo = 100, health = 0, temphealth = 0, newhealth = 1000;
+    ReadProcessMemory(hprocess, (BYTE *)healthaddr, &temphealth, sizeof(health), 0);
+    ReadProcessMemory(hprocess, (BYTE *)ammoaddr, &tempammo, sizeof(ammo), 0);
 
     while(1)
     {
+         ReadProcessMemory(hprocess, (BYTE *)healthaddr, &health, sizeof(health), 0);
          ReadProcessMemory(hprocess, (BYTE *)ammoaddr, &ammo, sizeof(ammo), 0);
          ammoaddr = findDMAaddr(hprocess, dynamicptr, ammos);
 
          Sleep(50);
-         if (ammo != temp)
+         if (ammo != tempammo)
          {
-             temp = ammo;
-             std::cout<<ammo<<'\n';
-
+             std::cout<<"Old ammo: "<<tempammo<<'\n';
+             tempammo = ammo;
              WriteProcessMemory(hprocess,(BYTE *)ammoaddr,&newammo, sizeof(newammo),0);
-
+             std::cout<<"New ammo: "<<newammo<<'\n';
+         }
+         if (temphealth != health)
+         {
+             std::cout<<"Old Health: "<<temphealth<<'\n';
+             temphealth = health;
+             WriteProcessMemory(hprocess,(BYTE *)healthaddr,&newhealth, sizeof(newhealth),0);
+             std::cout<<"New Health: "<<newhealth<<'\n';
          }
 
     }
